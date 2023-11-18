6c41c9be3c1ae0995544300f3cd388b5d382c938
Shivam Sundriyal <shivamsundriyal22@gmail.com>
2023-11-18 22:45:07 +0530

    First Project Completed

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1
+let accountEmail = "abc@gmail.com"
+var accountPassword = "123"             // Preferably not used
+
+accountCity = "Delhi"
+
+// accountId = 2            ---------> Not allowed
+// console.log(accountId)
+
+accountEmail = "123@gmail.com"
+accountPassword = "abc"
+accountCity = "Mumbai"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
