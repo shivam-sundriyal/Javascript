ed234d18c0b71b05cf05ff2cdd5d0215ada0c438
Shivam Sundriyal <shivamsundriyal22@gmail.com>
2023-11-29 19:44:37 +0530

    Twelfth

---
+++ Projects/12-DevPen/script.js
@@ -0,0 +1,16 @@
+let output = document.getElementById("output");
+
+const areas = document.querySelectorAll(".box");
+
+areas.forEach((area) => {
+  area.addEventListener("keyup", () => {
+    let htmlCode = document.getElementById("html-code").value;
+    let cssCode = document.getElementById("css-code").value;
+    let jsCode = document.getElementById("js-code").value;
+
+    output.contentDocument.body.innerHTML =
+      htmlCode + "<style>" + cssCode + "</style>";
+
+    output.contentWindow.eval(jsCode);
+  });
+});
