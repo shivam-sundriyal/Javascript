47d9946611e930983b5b1c7b552dd5f7cf50795a
Shivam Sundriyal <shivamsundriyal22@gmail.com>
2023-12-05 20:32:29 +0530

    First Project Completed

--- Projects/01-colorChanger/script.js
+++ Projects/01-colorChanger/script.js
@@ -1,11 +1,14 @@
 const buttons = document.querySelectorAll(".button");
 const body = document.querySelector("body");
 
-buttons.forEach(function(btn) {
-  btn.addEventListener("click", function(e) {
+buttons.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
     console.log(e);
     if (e.target.id === "grey") body.style.backgroundColor = e.target.id;
-    if (e.target.id === "white") body.style.backgroundColor = e.target.id;
+    if (e.target.id === "white") {
+      body.style.backgroundColor = e.target.id;
+      body.style.color = black;
+    }
     if (e.target.id === "blue") body.style.backgroundColor = e.target.id;
     if (e.target.id === "yellow") body.style.backgroundColor = e.target.id;
   });
